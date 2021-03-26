fd0927b84655bbd135d70c71b7229aaa32e898fc
petracattano <80822620+petracattano@users.noreply.github.com>
2021-03-26 18:30:25 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -114,8 +114,63 @@ clnir <- colorRampPalette(c("yellow","orange","green")) (100)
 plot(p224r63_2011$B4_sre, col=clnir) # per l'infrarosso
 
 
+# DAI 4 --> PLOT RGB
+
+# Funzione library per richiamare il pacchetto raster 
+library(raster)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") 
+
+# Funzione brick per importare i dati
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
 
+#Bande di Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico (lontano)
+# B7: infrarosso medio
 
+#plot RGB per visualizzare immagine a colori "naturali"
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+#Voglio vedere anche l'infrarosso vicino
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# monto la banda 4 dell'infrarosso vicino sul verde
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+# monto la banda 4 nel blu 
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+
+# le visualizzo tutte e quattro grazie alla funzione par, 2x2 multiframe
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+# per salvare come pdf il multiframe --> funzione pdf
+pdf("ll_mio_primo_pdf_con_R.pdf") 
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+# stretch histagram, più aggressivo, pendenza più potente
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+# par per immagine a colori naturali, immagine con infrarosso sul green, immagine con infrarosso sul green e his stretch. a 3 righe e 1 colonna.
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
 
