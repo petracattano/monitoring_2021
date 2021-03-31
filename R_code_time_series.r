7688d83ecc44324735f4b2dba04a4dea380ffaa5
petracattano <80822620+petracattano@users.noreply.github.com>
2021-03-31 13:01:06 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,12 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emnuela Cosma
+
+#richiama il pacchetto raster
+library(raster)
+
+# Cambio la working directory
+setwd("C:/lab/greenland")
+
+
+
