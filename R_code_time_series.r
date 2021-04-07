9da6dd001de0c62cab1112ed4c948eefc10d773e
petracattano <80822620+petracattano@users.noreply.github.com>
2021-04-07 13:05:51 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -8,5 +8,45 @@ library(raster)
 # Cambio la working directory
 setwd("C:/lab/greenland")
 
+#importo il primo dataset con la funzione raster (non uso brick perchè ho singoli file e non pacchetti)
+lst_2000 <- raster("lst_2000.tif")
+
+#lo plotto
+plot(lst_2000)
+
+#importo anche il dato del 2005
+lst_2005 <- raster("lst_2005.tif")
+
+#lo plotto
+plot(lst_2005)
+
+ #importo anche le altre immagini
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+#crea un multipannel
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+#funzioni per applicare funzioni a più file
+#devo creare una lista di file --> tramite la funzione list.files
+rlist <- list.files(pattern="lst")
+rlist
+
+#funzione lapply per applicare funzione raster su tutta la lista
+import <- lapply(rlist,raster)
+import
+
+#funzione stack 
+TGr <- stack(import)
+plot(TGr)
+
+
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+
+library(rasterVis)
 
 
