b5e58463d5e3ee2d628946d9dcf72d460501a68a
petracattano <80822620+petracattano@users.noreply.github.com>
2021-04-16 09:37:00 +0200

    Create R_code_knit.r

---
+++ R_code_knit.r
@@ -0,0 +1,11 @@
+# 16/04/2021
+# R_code_knitr.r
+# set della working directory 
+setwd("C:/lab/") # Windows
+
+#installo il pacchetto knitr
+install.packages("knitr")
+
+# carico il pacchetto knitr
+library(knitr)
+
