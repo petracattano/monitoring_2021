d1904bb61d6338274224283694ea8efa00a2b53f
petracattano <80822620+petracattano@users.noreply.github.com>
2021-04-23 17:07:19 +0200

    Update R_code_knit.r

--- R_code_knit.r
+++ R_code_knit.r
@@ -10,5 +10,8 @@ install.packages("knitr")
 #richiamo il pacchetto knitr
 library(knitr)
 
-
+#salva il codice di cui vuoi il report sul pc tramite qualsiasi gestore di testo (anche word è ok)
+#funzione stitch per applicare il pacchetto knitr al codice di cui voglio il report
+#nel primo argomento specifica l'estensione se hai problemi durante il caricamento (quì è .tex)
+stitch("R_code_greenland.r.tex", template=system.file("misc","knitr-template.Rnw", package="knitr"))
 
