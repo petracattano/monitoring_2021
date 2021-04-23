7605e833f7ce3ff64874fb66fbaa05d2d2811ed7
petracattano <80822620+petracattano@users.noreply.github.com>
2021-04-23 16:16:32 +0200

    Update R_code_knit.r

--- R_code_knit.r
+++ R_code_knit.r
@@ -1,15 +1,14 @@
 # 16/04/2021
-# R_code_knitr.r
+# R_code_knitr.r, codice per creare template finali
 
 # set della working directory 
 setwd("C:/lab/") # Windows
 
-#installo il pacchetto knitr
+#installo il pacchetto knitr, mi permette di creare dei report finali delle operazioni su R
 install.packages("knitr")
 
-# carico il pacchetto knitr
+#richiamo il pacchetto knitr
 library(knitr)
-
 
 
 
