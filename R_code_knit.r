a7aa727c0c9c26508606196c0eb9d6749559624c
petracattano <80822620+petracattano@users.noreply.github.com>
2021-06-18 08:16:23 +0200

    Update R_code_knit.r

--- R_code_knit.r
+++ R_code_knit.r
@@ -1,17 +1,17 @@
-# 16/04/2021
-# R_code_knitr.r, codice per creare template finali
+# R_code_knitr.r
+# codice per creare template finali, report di codici
 
 # set della working directory 
-setwd("C:/lab/") # Windows
+setwd("C:/lab/")
 
-#installo il pacchetto knitr, mi permette di creare dei report finali delle operazioni su R
+# installo il pacchetto knitr, mi permette di creare dei report finali delle operazioni su R
 install.packages("knitr")
 
-#richiamo il pacchetto knitr
+# richiamo il pacchetto knitr
 library(knitr)
 
-#salva il codice di cui vuoi il report sul pc tramite qualsiasi gestore di testo (anche word è ok)
-#funzione stitch per applicare il pacchetto knitr al codice di cui voglio il report
-#nel primo argomento specifica l'estensione se hai problemi durante il caricamento (quì è .tex)
+# salva il codice di cui vuoi il report sul pc tramite qualsiasi gestore di testo (anche word)
+# funzione stitch per applicare il pacchetto knitr al codice di cui voglio il report
+# nel primo argomento specifica l'estensione se hai problemi durante il caricamento (quì è .tex)
 stitch("R_code_greenland.r.tex", template=system.file("misc","knitr-template.Rnw", package="knitr"))
 
