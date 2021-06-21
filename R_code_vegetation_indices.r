3dab59fbdd417bdefbe66210fbb038d620e90b47
petracattano <80822620+petracattano@users.noreply.github.com>
2021-06-21 10:18:42 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -2,11 +2,9 @@
 
 # richiamo le librarie necessarie
 library(raster)
-library(RStoolbox)
+library(RStoolbox) # per la funzione spectrakindices
 library(rasterVis)
-
-# installo il pacchetto rasterdiv per usufrire del dataset sugli indici di vegetazione
-install.packages("rasterdiv")
+install.packages("rasterdiv") # installo il pacchetto rasterdiv per usufrire del dataset sugli indici di vegetazione
 library(rasterdiv)
 
 # settaggio della working directory dove ho salvato i dati
@@ -16,18 +14,18 @@ setwd("C:/lab")
 defor1 <- brick("defor1.jpg") # virgolette perchè esco da R
 defor2 <- brick("defor2.jpg")
 
-#plot RGB e faccio un par per visualizzare la multitemporalità
+#plot RGB e faccio un par 2x1 per visualizzare la multitemporalità
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin") 
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
 
-# voglio clcolare il DVI, difference vegetation index. E' un indice di vegetazione ed è uguale alla differenza tra la riflettanza nell’infrarosso vicino e quella nel rosso 
+# voglio calcolare il DVI, difference vegetation index. E' un indice di vegetazione ed è uguale alla differenza tra la riflettanza nell’infrarosso vicino e quella nel rosso 
 # dobbiamo fare il NIR-RED della defor 1
 # scrivo defor1 per vedere le informazioni dei file e i nomi delle bande
 defor1
 
-## band1: NIR, defor1_.1
+# band1: NIR, defor1_.1
 # band2: red, defor1_.2
 
 
@@ -71,7 +69,7 @@ plot(difdvi, col=cld) # parte in rosso dove la differenza è + alta (molta defor
 
 
 # il DVI può essere normalizzato per ottenere l'NDVI
-# i valori di riflettanza dipendono dai bit. Nel calcolo del DVI posso confrontare solo immagini con gli stessi bit. NDVI assume valori che vanno da -1 a 1 e permette confronti
+# i valori di riflettanza dipendono dai bit. Nel calcolo del DVI posso confrontare solo immagini con gli stessi bit. NDVI assume valori che vanno da -1 a 1 e permette confronti anche tra immagini a bit differenti
 # NDVI= (NIR-RED)/(NIR+RED)
 # NDVI1, calcolo sulla prima immagine
 ndvi1 <- (defor1$defor1.1-defor1$defor1.2)/(defor1$defor1.1+defor1$defor1.2)
@@ -88,7 +86,7 @@ plot(ndvi2, col=cl,  main="NDVI at time 2")
 
 # posso ottenere gli stessi dati dalla funzione spectrakindices del pacchetto RStoolbox, mi permette di fare calcoli in modo + spedito
 # la funzione calcola tutta una serie di indici tra cui NDVI
-vi <-spectralIndices(defor1, green=3,red=2,nir=1)
+vi <-spectralIndices(defor1, green=3,red=2,nir=1) #devo indicare le bande
 plot(vi, col=cl)
 
 #fai lo stesso per la seconda immagine
@@ -97,17 +95,18 @@ plot(vi2, col=cl)
 
 #05/05/2021
 
-# worldwide NDVI, visualizziamo il database copNDVI del pacchetto rasterdiv
-# mappa NDVI a scala globale
+# worldwide NDVI, visualizziamo il database copNDVI (dati copernicus 1999-2017) del pacchetto rasterdiv per ottenere una mappa NDVI a scala globale
+# plotto il dataset, ottengo mappa NDVI a scala globale
 plot(copNDVI)
 
-# non voglio visualizzare l'acqua 
-# posso eliminare i valori del database corrispondenti tramite la funzione reclassify, argomento cbind
+# nella mappa non mi interessa visualizzare l'acqua, appesantisce la visualizzazione 
+# posso eliminare i valori del database corrispondenti ai pixel di acqua tramite la funzione reclassify, argomento cbind
 # i pixel con valori da 253 a 255 verranno classificati come NA, non valori
-copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA)) # ignoro dei valori specifici tramite l'argomento cbind
 plot(copNDVI)
 
-# funzione level plot per visualizzare la varianza in termini di biomassa
+# funzione level plot per visualizzare la varianza in termini di biomassa sul grafico
+# grafico più potente e informativo
 # abbiamo una visione dell'estensione della biomassa, ricorda la distribuzione dei biomi 
 levelplot(copNDVI)
 
